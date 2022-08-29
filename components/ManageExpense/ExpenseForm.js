fe4e1967eb2d82714f652666efedec4a29b44eaa
Nhan Phan <kippulainenko@gmail.com>
2022-08-30 00:09:46 +0300

    use generic way to update input state

--- components/ManageExpense/ExpenseForm.js
+++ components/ManageExpense/ExpenseForm.js
@@ -1,9 +1,21 @@
+import { useState } from "react"
 import { StyleSheet, Text, View } from "react-native"
 import Input from "./Input"
 
 const ExpenseForm = () => {
-    function amountChangedHanlder() {
+    const [inputValue, setInputValue] = useState({
+        amount: '',
+        date: '',
+        description: ''
+    });
 
+    function inputChangedHanlder(inputIdentifier, enteredValue) {
+        setInputValue((curInputValues) => {
+            return {
+                ...curInputValues,
+                [inputIdentifier]: enteredValue
+            }
+        })
     }
 
   return (
@@ -11,11 +23,13 @@ const ExpenseForm = () => {
         <Text style={styles.title}>Your Expense</Text>
         <View style={styles.inputsRow}>
             <Input
-            style={styles.rowInput} 
+            style={styles.rowInput}
             label='Amount' 
             textInputCongig={{
                 keyboardType: 'decimal-pad',
-                onChangeText: amountChangedHanlder,
+                onChangeText: inputChangedHanlder.bind(this, 'amount'), 
+                // preconfigure a func for future execution by using bind()
+                value: inputValue.amount
             }}
             />
             <Input
@@ -24,7 +38,8 @@ const ExpenseForm = () => {
             textInputCongig={{
                 placeholder: 'YYYY-MM-DD',
                 maxLength: 10,
-                onChangeText: () => {}
+                onChangeText: inputChangedHanlder.bind(this, 'date'), 
+                value: inputValue.date
             }}
             />
         </View>
@@ -34,6 +49,8 @@ const ExpenseForm = () => {
             multiline: true,
             // autoCorrect: false
             // autoCapitalize: 'none'
+            onChangeText: inputChangedHanlder.bind(this, 'description'), 
+            value: inputValue.description
         }}
         />
     </View>
